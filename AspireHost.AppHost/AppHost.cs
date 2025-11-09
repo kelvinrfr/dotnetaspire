84c50467f83d56bcd2213b313e71fcf7e015707b
Kelvin Ferreira <10376375+kelvinrfr@users.noreply.github.com>
2025-11-09 01:54:47 -0500

    adding telemetry support

--- AspireHost.AppHost/AppHost.cs
+++ AspireHost.AppHost/AppHost.cs
@@ -4,6 +4,7 @@ var cache = builder.AddRedis("cache")
     .WithDataVolume(); // Persists a volume for a known resource
 
 builder.AddProject<Projects.TodoApi>("api")
+    .WithHttpHealthCheck(path: "/healthz", statusCode: 200)
     .WithReference(cache)
     .WithEnvironment("ConnectionStrings__Redis", cache.Resource.ConnectionStringExpression) // Overwrites the environment variable
     .WaitFor(cache);
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using TodoApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +27,36 @@ builder.Services.AddStackExchangeRedisCache(options =>
 // Register application services
 builder.Services.AddScoped<ITodoService, TodoService>();
 
+builder.Logging.AddOpenTelemetry(logging =>
+{
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
+});
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        metrics.AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddRuntimeInstrumentation();
+    })
+    .WithTracing(tracing =>
+    {
+        tracing.AddSource(builder.Environment.ApplicationName)
+            .AddAspNetCoreInstrumentation(tracing =>
+                // Exclude health check requests from tracing
+                tracing.Filter = context => !context.Request.Path.StartsWithSegments("/healthz")
+            )
+            // Uncomment the following line to enable gRPC instrumentation (requires the OpenTelemetry.Instrumentation.GrpcNetClient package)
+            //.AddGrpcClientInstrumentation()
+            .AddHttpClientInstrumentation();
+    });
+var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+if (useOtlpExporter)
+{
+    builder.Services.AddOpenTelemetry().UseOtlpExporter();
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,5 +68,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 app.MapControllers();
+
+// Mocking a health check implementation
+app.MapGet("/healthz", () => Results.Ok("Ok"));
 
 app.Run();
